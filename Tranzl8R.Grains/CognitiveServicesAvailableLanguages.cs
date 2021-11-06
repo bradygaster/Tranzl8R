681b55d3b1f167011c2061d67c345f79e200f663
bradygaster <bradyg@microsoft.com>
2021-11-06 02:00:34 -0700

    doh

--- Tranzl8R.Grains/CognitiveServicesAvailableLanguages.cs
+++ Tranzl8R.Grains/CognitiveServicesAvailableLanguages.cs
@@ -17,7 +17,7 @@ namespace Tranzl8R
         public async Task<List<LanguageItem>> GetAllLanguages()
         {
             using (var client = HttpClientFactory.CreateClient())
-            using (var request = HttpClientFactory.CreateClient().SetupTranslatorRequestFromConfiguration(Configuration, "/languages?api-version=3.0&scope=translation"))
+            using (var request = client.SetupTranslatorRequestFromConfiguration(Configuration, "/languages?api-version=3.0&scope=translation"))
             {
                 request.Method = HttpMethod.Get;
                 HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
