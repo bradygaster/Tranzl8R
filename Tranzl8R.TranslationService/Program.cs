344c7e52cf76dc5940dab16c2f417b59a85af940
bradygaster <bradyg@microsoft.com>
2021-11-15 12:15:50 -0800

    trying to get worker working

--- Tranzl8R.TranslationService/Program.cs
+++ Tranzl8R.TranslationService/Program.cs
@@ -1,9 +1,53 @@
 using Tranzl8R.TranslationService;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using System.Net;
 
-IHost host = Host.CreateDefaultBuilder(args)
+IHost host = Host
+    .CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
+    })
+    .ConfigureAppConfiguration(configurationBuilder =>
+    {
+        configurationBuilder.AddUserSecrets<Worker>();
+    })
+    .UseOrleans((hostBuilderContext, siloBuilder) =>
+    {
+        var storageConnectionString = hostBuilderContext.Configuration.GetValue<string>("AZURE_STORAGE_CONNECTION_STRING");
+
+        IPAddress endpointAddress = IPAddress.Parse(hostBuilderContext.Configuration.GetValue<string>("WEBSITE_PRIVATE_IP"));
+
+        var strPorts = hostBuilderContext.Configuration.GetValue<string>("WEBSITE_PRIVATE_PORTS").Split(',');
+
+        int siloPort = int.Parse(strPorts[0]);
+        int gatewayPort = int.Parse(strPorts[1]);
+        int delta = 11;
+
+        siloBuilder
+            .Configure<SiloOptions>(options => options.SiloName = "Worker Service")
+            .Configure<ClusterOptions>(clusterOptions =>
+            {
+                clusterOptions.ClusterId = "Cluster";
+                clusterOptions.ServiceId = "Service";
+            })
+            .Configure<EndpointOptions>(endpointOptions =>
+            {
+                endpointOptions.AdvertisedIPAddress = IPAddress.Loopback;
+                endpointOptions.SiloPort = siloPort + delta;
+                endpointOptions.GatewayPort = gatewayPort + delta;
+            })
+            .UseAzureStorageClustering(storageOptions =>
+            {
+                storageOptions.ConnectionString = storageConnectionString;
+            })
+            .AddAzureTableGrainStorageAsDefault(tableStorageOptions =>
+            {
+                tableStorageOptions.ConnectionString = storageConnectionString;
+                tableStorageOptions.UseJson = true;
+            });
     })
     .Build();
 
--- Tranzl8R.TranslationService/Worker.cs
+++ Tranzl8R.TranslationService/Worker.cs
@@ -1,12 +1,28 @@
+using Orleans;
+
 namespace Tranzl8R.TranslationService
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IGrainFactory grainFactory;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IGrainFactory grainFactory)
         {
             _logger = logger;
+            this.grainFactory = grainFactory;
+        }
+
+        public override async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var translationServerGrain = grainFactory.GetGrain<ITranslationServer>(Guid.Empty);
+            (await translationServerGrain.GetAllLanguages()).ForEach(async (language) =>
+            {
+                await translationServerGrain.ToggleLanguageActiveStatus(language.Code);
+                _logger.LogInformation($"Translator for {language.Code} ready.");
+            });
+
+            await base.StartAsync(cancellationToken);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
